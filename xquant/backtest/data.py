9c6b0200d121e699f0774743dc29282adddf79d4
Percival Dazheng Xu <percival1205@gmail.com>
2021-04-28 14:26:30 +0800

    added data class

--- xquant/backtest/data.py
+++ xquant/backtest/data.py
@@ -1,3 +1,13 @@
 # TODO
+import typing
+
 # implement Data class that loads data
 # implement getters (and maybe setters???)
+
+class data():
+
+    # constructor
+    def __init__(self, data) -> None:
+        '''
+        data
+        '''
