383c2967c7f0ba654ac04dd478ea3c5d8a25b31a
Percival Dazheng Xu <percival1205@gmail.com>
2021-04-28 16:02:25 +0800

    implemented setter and getter

--- xquant/backtest/data.py
+++ xquant/backtest/data.py
@@ -1,13 +1,31 @@
 # TODO
-import typing
+import pandas as pd
 
-# implement Data class that loads data
-# implement getters (and maybe setters???)
-
-class data():
+class Data():
 
     # constructor
-    def __init__(self, data) -> None:
+    def __init__(self, data:dict) -> None:
         '''
-        data
+        Parameters
+        ----------
+        data : a dict with dataset names as keys and pandas DataFrames as values
+
+        Example
+        -------
+        `
+        data = Data({'prices':df_prices, 'volume':df_volume})
+        `
         '''
+        assert isinstance(data, dict) and bool(
+            data), 'data must be a non-empty dictionary'
+        self.data = data
+
+    def get_data(self, key:str) -> pd.DataFrame:
+        '''getter function'''
+        assert key in self.data.keys(), 'attribute "data" has no matching key'
+        return self.data[key]
+
+    def set_data(self, key:str, df:pd.DataFrame) -> None:
+        '''setter function'''
+        assert isinstance(df, pd.DataFrame), 'df must be a pandas DataFrame'
+        self.data[key] = df
