a22859719cbb9a0255b775516fe80482fb4e0cba
Percy Xu <percival1205@gmail.com>
2021-05-11 10:43:02 +0800

    created strategy abstract base class (finally...)

---
+++ xquant/strategy.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+from xquant.portfolio import Portfolio
+
+class Strategy(ABC):
+    '''
+    Strategy
+    --------
+    An abstract base class serves as the parent class of the user's strategy. 
+    The user must create a child class and implement the stock_selection method.
+    '''
+
+    def __init__(self, strategy_name) -> None:
+        self.strategy_name = strategy_name
+
+    @abstractmethod
+    def stock_selection() -> Portfolio:
+        '''selects a portfolio of stocks and shares from the stock selection universe'''
+        pass
