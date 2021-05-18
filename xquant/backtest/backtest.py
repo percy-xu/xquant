a4e8f578206c21f45dcba0bdcde1815812c0a32f
Percy Xu <percival1205@gmail.com>
2021-05-18 18:02:45 +0800

    implemented driver code

--- xquant/backtest/backtest.py
+++ xquant/backtest/backtest.py
@@ -2,15 +2,16 @@ from xquant.backtest.holdings import Holdings
 from xquant.util.period import Period
 from dateutil.relativedelta import relativedelta
 
-
 def run_backtest(start_time, end_time, stock_selection, funds, rebalance_freq) -> Holdings:
-    holdings = Holdings()
+    holdings = Holdings(holdings={})
     now = start_time
     while now < end_time:
+        print(f'Now processing: {now}')
         portfolio = stock_selection(funds=funds, date=now)
         next_rebalance = now + relativedelta(months=rebalance_freq)
         current_period = Period(now, next_rebalance-relativedelta(days=1))
         holdings.holdings[current_period] = portfolio
+        now = next_rebalance
     
     return holdings
 
