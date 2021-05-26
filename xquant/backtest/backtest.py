c76186ae302ff7541b016b848dc67f875ad3dfa8
Percy Xu <percival1205@gmail.com>
2021-05-26 13:59:54 +0800

    added documentation

--- xquant/backtest/backtest.py
+++ xquant/backtest/backtest.py
@@ -3,6 +3,24 @@ from xquant.util.period import Period
 from dateutil.relativedelta import relativedelta
 
 def run_backtest(start_time, end_time, df_price, stock_selection, init_funds, rebalance_freq) -> Holdings:
+    '''
+    Driver code that initiates the back-testing process
+
+    Parameters
+    ----------
+    start_time : datetime.datetime or pandas.Timestamp
+        Start time for the back-test period
+    end_time : datetime.datetime or pandas.Timestamp
+        End time for the back-test period
+    df_price : pandas.DataFrame
+        Prices dataframe with DatetimeIndex and ticker symbols as columns
+    stock_selection : a function (see xquant.strategy.Strategy.stock_selection)
+        A function that selects stocks for inclusion in a cross-section
+    init_funds : int or float
+        The attributtable funds at start_time
+    rebalance_freq: int
+        Rebalance frequency, in months
+    '''
     holdings = Holdings(holdings={})
     now = start_time
     last_portfolio = None
@@ -20,4 +38,3 @@ def run_backtest(start_time, end_time, df_price, stock_selection, init_funds, re
         last_portfolio = portfolio
     
     return holdings
-
